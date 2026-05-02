074afa3a47ea84313f9467b723b590adbdae1fde
ThayglaGC <thayglag@gmail.com>
2026-05-02 12:50:41 -0300

    Teste de console vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+console.log(import.meta.env);
+
 export default defineConfig({
   plugins: [react()],
   base: "/turma-delta/",
