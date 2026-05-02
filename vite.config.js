f73d15289b29a633d9bb8a90c355d7a6add50ad8
ThayglaGC <thayglag@gmail.com>
2026-05-02 12:09:29 -0300

    mudança no vite

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/turma-delta/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
